9efb4fee3e9e5055b59ed17681c1707193825300
jkgs1 <john.kagstrom@gmail.com>
2023-02-22 17:13:16 +0100

    först guin

--- GUI.java
+++ GUI.java
@@ -1,1 +1,38 @@
-//Test
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.GridBagLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+public class GUI extends JFrame{
+    JFrame frame = new JFrame();
+   // this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JPanel canvas = new JPanel();
+    public GUI(){
+        ritaCanvas();
+    }
+
+    private void ritaCanvas(){
+        canvas.setBounds(2000,2000,2000,2000);
+        canvas.setBackground(Color.RED);
+        frame.getContentPane().add(canvas);
+        this.setVisible(true);
+        this.pack();
+    }
+
+   private void ritaDisplay(){
+
+    }
+
+    private void ritaKeypad(){
+
+    }
+
+    private void ritaKnappar(){
+
+    }
+
+
+}
+
