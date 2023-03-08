8d978273582d4934e074ea18055594ce8757a5ec
Buchts <123960735+Buchts@users.noreply.github.com>
2023-03-08 15:39:17 +0100

    Update DigitButton.java

--- DigitButton.java
+++ DigitButton.java
@@ -1,8 +1,37 @@
-public class DigitButton extends CalculatorButton{
-    public DigitButton(String enStr, Situation situation){
-        super(enStr, situation);
+public class DigitButton extends CalculatorButton {
+
+    public DigitButton(String enStr, Situation situation) {
+
+        super(enStr,situation); // constructor
+
     }
-    
-    public void transition(){
+
+    @Override
+    public void transition() {
+
+        switch (this.situation.state) { //switch
+            case OpReady:
+                this.situation.setDisplay("integer 0"); // GUI method setDisplay
+                this.situation.state = State.Input2;
+                break;
+            case HasResult:
+                this.situation.setDisplay("integer 0"); // GUI method setDisplay
+                this.situation.state = State.Input1;
+                break;
+            case Input1:
+                this.situation.state = State.Input1;
+                this.situation.setDisplay();
+                break;
+            case Input2:
+                this.situation.state = State.HasReslut;
+                this.setBGColor(Color.GRAY);
+                ApplyAsIntmetod(a,b);
+                GUI.setDisplay( // result of binary operator);
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
