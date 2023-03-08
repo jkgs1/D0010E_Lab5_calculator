2c6d864d648ffe12067b2c75008ed69b1d079f47
Buchts <123960735+Buchts@users.noreply.github.com>
2023-03-08 15:38:40 +0100

    Update EqualsButton.java

--- EqualsButton.java
+++ EqualsButton.java
@@ -1,3 +1,37 @@
-public class EqualsButton extends CalculatorButton{
+import java.awt.Color;
+
+public class EqualsButton extends CalculatorButton {
+
+    public EqualsButton(String enStr, Situation situation) {
+
+        super(enStr,situation); // constructor
+
+    }
     
+    @Override
+    public void transition() {
+        
+        switch (this.situation.state) { //switch
+            case OpReady:
+                this.situation.state = State.OpReady;
+                break;
+            case HasResult:
+                this.situation.state = State.HasReslut;
+                break;
+            case Input1:
+                this.situation.state = State.Input1;
+                break;
+            case Input2:
+                this.situation.state = State.HasReslut;
+                this.setBGColor(Color.GRAY);
+                ApplyAsIntmetod(a,b);
+                this.situation.setDisplay(); // GUI method setDisplay
+                
+                break;
+
+            default:
+                break;
+        }
+
+    }
 }
